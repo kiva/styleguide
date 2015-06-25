7652d2b928519813446249224bea9e78d3441314
Sam Mankiewicz <sam@kiva.org>
2015-06-25 01:19:34 -0700

    conform to new rules

--- bin/bamboo_functions.sh
+++ bin/bamboo_functions.sh
@@ -7,18 +7,20 @@
 
 
 build_patternlab () {
-	# public/ is in .gitignore; make sure it exists first
-	mkdir -p public
-	php core/builder.php --generate 
+#	#public/ is in .gitignore; make sure it exists first
+#	mkdir -p public
+	npm install
 }
 
 x_rsync="rsync -e ssh -avP --delete --exclude .git "
 
+# stage_code $user $host
 stage_code () {
 	# rsync
-	$x_rsync public/ ${1}@{$2}:/var/www/${3}
+	${x_rsync} public/ ${1}@{$2}:/var/www/styleguide
 }
 
+# enable_vhost $user $host
 enable_vhost () {
 	ssh -T ${1}@${2} "sudo a2ensite styleguide"
 }
