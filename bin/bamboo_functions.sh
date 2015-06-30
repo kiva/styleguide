bf1ee12714b73358c60f97aa32accc6c1bc293e5
Sam Mankiewicz <sam@kiva.org>
2015-06-29 18:21:24 -0700

    TO-897 again

--- bin/bamboo_functions.sh
+++ bin/bamboo_functions.sh
@@ -32,7 +32,7 @@ stage_code () {
 	# rsync
 	echo "Staging code onto '${1}'"
 	pushd ${base_dir}
-	${x_rsync} public/ ${2}@${1}:${docroot}
+	${x_rsync} public/* ${2}@${1}:${docroot}
 	# copy over bin/styleguide.conf too?
 	popd
 }
