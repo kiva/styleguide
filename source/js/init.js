3c4b2b85c787ac1a225b08e9cdf8bf109ab5b0ef
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2015-07-14 14:11:33 -0700

    REDO-90 small-screen search-box

--- source/js/init.js
+++ source/js/init.js
@@ -1,4 +1,4 @@
-(function(global, $){
+(function(global, $, Foundation){
 
 	//Navigation toggle
 	$('.nav-toggle-menu').click(function(e) {
@@ -37,4 +37,38 @@
 		$target.attr('aria-hidden', $target.attr('aria-hidden') === 'false');
 	});
 
-})(this, jQuery);
+	/* Search box JS */
+
+	var $search_toggle = $('#search-toggle');
+	var $close_search = $('#close-search');
+	var $search_form = $('#search-form');
+
+	$search_toggle.click(function(e) {
+		e.preventDefault();
+
+		var expanded = $search_toggle.attr('aria-expanded') === 'false';
+
+		$search_toggle.attr('aria-expanded', expanded);
+		$close_search.attr('aria-expanded', expanded);
+		$search_form.attr('aria-hidden', $search_form.attr('aria-hidden') === 'false');
+	});
+
+	$close_search.click(function(e) {
+		e.preventDefault();
+
+		$search_toggle.attr('aria-expanded', false);
+		$close_search.attr('aria-expanded', false);
+		$search_form.attr('aria-hidden', true);
+	});
+
+	// Throttled resize function
+	$(window).on('resize', Foundation.utils.throttle(function() {
+		if(Foundation.utils.is_small_only()) {
+			$close_search.attr('aria-hidden', false);
+		}
+		else {
+			$close_search.attr('aria-hidden', true);
+		}
+	}, 200));
+
+})(this, jQuery, Foundation);
