7ff8a37fdcf408a041b285248fab95e0ff1e9058
Abhishek Bhatnagar <abhishekbh@gmail.com>
2015-09-01 14:37:04 -0700

    enable equalizer in general

--- source/js/inc/app.js
+++ source/js/inc/app.js
@@ -4,7 +4,11 @@ var header = require('./app/header');
 var filters = require('./app/filters');
 var $ = require('jquery');
 
-$(document).foundation();
+$(document).foundation({
+	equalizer: {
+		equalize_on_stack: true
+	}
+});
 
 header();
 filters();
