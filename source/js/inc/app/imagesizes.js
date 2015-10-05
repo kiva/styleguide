c78cd2bb8a3fee397ae7cb6ffe9d81c3d44c9f8f
Jim Edlin <jim@kiva.org>
2015-10-05 13:23:33 -0700

    REDO-411 - simplified retina queries

--- source/js/inc/app/imagesizes.js
+++ source/js/inc/app/imagesizes.js
@@ -25,68 +25,28 @@ module.exports = function () {
 			'xxlarge': 'only screen and (min-width: 1441px)' /* 1441px */
 			,
 			'small-only-retina': 'only screen and (max-width: 480px) and (-webkit-min-device-pixel-ratio: 2)'
-			+ ', only screen and (max-width: 480px) and (min--moz-device-pixel-ratio: 2)'
-			+ ', only screen and (max-width: 480px) and (-o-min-device-pixel-ratio: 2/1)'
-			+ ', only screen and (max-width: 480px) and (min-device-pixel-ratio: 2)'
 			+ ', only screen and (max-width: 480px) and (min-resolution: 192dpi)'
-			+ ', only screen and (max-width: 480px) and (min-resolution: 192dpi)'
-			+ ', only screen and (max-width: 480px) and ((min - resolution: 2dppx)'
 			,
-			'medium-retina': 'only screen and ((min-width: 481px) and (-webkit-min-device-pixel-ratio: 2)' /* 481px */
-			+ ', only screen and (min-width: 481px) and (min--moz-device-pixel-ratio: 2)'
-			+ ', only screen and (min-width: 481px) and (-o-min-device-pixel-ratio: 2/1)'
-			+ ', only screen and (min-width: 481px) and (min-device-pixel-ratio: 2)'
-			+ ', only screen and (min-width: 481px) and (min-resolution: 192dpi)'
-			+ ', only screen and (min-width: 481px) and (min-resolution: 192dpi)'
-			+ ', only screen and (min-width: 481px) and ((min - resolution: 2dppx)'
+			'medium-retina': 'only screen and (min-width: 481px) and (-webkit-min-device-pixel-ratio: 2)'
+			+ ', only screen and (min-width: 481px) and (min-resolution: 192dpi)' + ', only screen and (min-width: 481px) and (min-resolution: 192dpi)'
 			,
 			'medium-only-retina': 'only screen and (min-width: 481px) and (max-width: 680px) and (-webkit-min-device-pixel-ratio: 2)' /* 481px, 680px */
-			+ ', only screen and (min-width: 481px) and (max-width: 680px) and (min--moz-device-pixel-ratio: 2)'
-			+ ', only screen and (min-width: 481px) and (max-width: 680px) and (-o-min-device-pixel-ratio: 2/1)'
-			+ ', only screen and (min-width: 481px) and (max-width: 680px) and (min-device-pixel-ratio: 2)'
 			+ ', only screen and (min-width: 481px) and (max-width: 680px) and (min-resolution: 192dpi)'
-			+ ', only screen and (min-width: 481px) and (max-width: 680px) and (min-resolution: 192dpi)'
-			+ ', only screen and (min-width: 481px) and (max-width: 680px) and ((min - resolution: 2dppx)'
 			,
 			'large-retina': 'only screen and (min-width: 681px) and (-webkit-min-device-pixel-ratio: 2)' /* 681px */
-			+ ', only screen and (min-width: 681px) and (min--moz-device-pixel-ratio: 2)'
-			+ ', only screen and (min-width: 681px) and (-o-min-device-pixel-ratio: 2/1)'
-			+ ', only screen and (min-width: 681px) and (min-device-pixel-ratio: 2)'
 			+ ', only screen and (min-width: 681px) and (min-resolution: 192dpi)'
-			+ ', only screen and (min-width: 681px) and (min-resolution: 192dpi)'
-			+ ', only screen and (min-width: 681px) and ((min - resolution: 2dppx)'
 			,
 			'large-only-retina': 'only screen and (min-width: 681px) and (max-width: 1024px) and (-webkit-min-device-pixel-ratio: 2)' /* 681px, 1024px */
-			+ ', only screen and (min-width: 681px) and (max-width: 1024px) and (min--moz-device-pixel-ratio: 2)'
-			+ ', only screen and (min-width: 681px) and (max-width: 1024px) and (-o-min-device-pixel-ratio: 2/1)'
-			+ ', only screen and (min-width: 681px) and (max-width: 1024px) and (min-device-pixel-ratio: 2)'
 			+ ', only screen and (min-width: 681px) and (max-width: 1024px) and (min-resolution: 192dpi)'
-			+ ', only screen and (min-width: 681px) and (max-width: 1024px) and (min-resolution: 192dpi)'
-			+ ', only screen and (min-width: 681px) and (max-width: 1024px) and ((min - resolution: 2dppx)'
 			,
 			'xlarge-retina': 'only screen and (min-width: 1025px) and (-webkit-min-device-pixel-ratio: 2)' /* 1025px */
-			+ ', only screen and (min-width: 1025px) and (min--moz-device-pixel-ratio: 2)'
-			+ ', only screen and (min-width: 1025px) and (-o-min-device-pixel-ratio: 2/1)'
-			+ ', only screen and (min-width: 1025px) and (min-device-pixel-ratio: 2)'
 			+ ', only screen and (min-width: 1025px) and (min-resolution: 192dpi)'
-			+ ', only screen and (min-width: 1025px) and (min-resolution: 192dpi)'
-			+ ', only screen and (min-width: 1025px) and ((min - resolution: 2dppx)'
 			,
 			'xlarge-only-retina': 'only screen and (min-width: 1025px) and (max-width: 1440px) and (-webkit-min-device-pixel-ratio: 2)' /* 1024px, 1440px */
-			+ ', only screen and (min-width: 1025px) and (max-width: 1440px) and (min--moz-device-pixel-ratio: 2)'
-			+ ', only screen and (min-width: 1025px) and (max-width: 1440px) and (-o-min-device-pixel-ratio: 2/1)'
-			+ ', only screen and (min-width: 1025px) and (max-width: 1440px) and (min-device-pixel-ratio: 2)'
 			+ ', only screen and (min-width: 1025px) and (max-width: 1440px) and (min-resolution: 192dpi)'
-			+ ', only screen and (min-width: 1025px) and (max-width: 1440px) and (min-resolution: 192dpi)'
-			+ ', only screen and (min-width: 1025px) and (max-width: 1440px) and ((min - resolution: 2dppx)'
 			,
 			'xxlarge-retina': 'only screen and (min-width: 1441px) and (-webkit-min-device-pixel-ratio: 2)' /* 1441px */
-			+ ', only screen and (min-width: 1441px) and (min--moz-device-pixel-ratio: 2)'
-			+ ', only screen and (min-width: 1441px) and (-o-min-device-pixel-ratio: 2/1)'
-			+ ', only screen and (min-width: 1441px) and (min-device-pixel-ratio: 2)'
 			+ ', only screen and (min-width: 1441px) and (min-resolution: 192dpi)'
-			+ ', only screen and (min-width: 1441px) and (min-resolution: 192dpi)'
-			+ ', only screen and (min-width: 1441px) and ((min - resolution: 2dppx)'
 		}
 	});
 
