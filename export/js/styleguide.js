9b81a128275e9e77a9f05589c00f61d0f8752fe1
Abhishek Bhatnagar <abhishekbh@gmail.com>
2015-10-12 03:00:38 -0700

    release 1.0.136

--- export/js/styleguide.js
+++ export/js/styleguide.js
@@ -22780,64 +22780,6 @@ define("Styleguide", ["jquery"], function(__WEBPACK_EXTERNAL_MODULE_2__) { retur
 
 		// Open Filters By Default
 		$('.off-canvas-wrap').foundation('offcanvas', 'show', 'move-right');
-
-		// Keywords Search Box
-		$('#filter-keywords-search-box').click(function(){
-			if ($(this).val() === 'Borrower name, description') {
-				$(this).val('');
-			}
-		});
-		$('#filter-keywords-search-box').focusout(function(){
-			if ($(this).val() === '') {
-				$(this).val('Borrower name, description');
-			}
-		});
-
-		// NoUiSlider
-		$('.risk-rating-slider').noUiSlider({
-			start: [ 0, 5 ],
-			connect: true,
-			range: {
-				'min': 0,
-				'max': 5
-			}
-		});
-
-		$('.delinquency-rate-slider').noUiSlider({
-			start: [ 0, 99 ],
-			connect: true,
-			range: {
-				'min': 0,
-				'max': 99
-			}
-		});
-
-		$('.default-rate-slider').noUiSlider({
-			start: [ 0, 99 ],
-			connect: true,
-			range: {
-				'min': 0,
-				'max': 99
-			}
-		});
-
-		$('.borrower-cost-slider').noUiSlider({
-			start: [ 0, 99 ],
-			connect: true,
-			range: {
-				'min': 0,
-				'max': 99
-			}
-		});
-
-		$('.profitability-slider').noUiSlider({
-			start: [ 0, 99 ],
-			connect: true,
-			range: {
-				'min': 0,
-				'max': 99
-			}
-		});
 	};
 
 
