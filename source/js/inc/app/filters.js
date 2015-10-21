9369d15e3a472569d30cd9eb0c6f4809f5342138
Abhishek Bhatnagar <abhishekbh@gmail.com>
2015-10-20 17:03:30 -0700

    a couple of fixrs

--- source/js/inc/app/filters.js
+++ source/js/inc/app/filters.js
@@ -4,23 +4,36 @@ module.exports = function() {
 	var $ = require('jquery');
 	var Foundation = require('Foundation');
 
-	$(document).on('open.fndtn.offcanvas', '[data-offcanvas]', function () {
-		if ($('#filter-sectors-ul').height() > 0){
-			$('#sectors-accordion-selector').trigger('click');
-		}
+	if (Foundation.utils.is_medium_up()){
+		$('.off-canvas-wrap').foundation('offcanvas', 'show', 'move-right');
+		$('#filter-menu-button').css('visibility','hidden');
+	}
 
-		if ($('#filter-attributes-ul').height() > 0){
-			$('#attributes-accordion-selector').trigger('click');
-		}
-
-		if ($('#filter-tags-ul').height() > 0){
-			$('#tags-accordion-selector').trigger('click');
-		}
-	});
+	if (Foundation.utils.is_small_only()){
+		$('.off-canvas-wrap').foundation('offcanvas', 'hide', 'move-right');
+		$('#filter-menu-button').css('visibility','initial');
+	}
 
 	// init the multi-select for partners
 	$('#partnersFilter').select2({
 		placeholder: 'Specify a partner'
 		, width: 'style'
+	});
+
+	// collapse all accordions if opening up offcanvas
+	$(document).on('open.fndtn.offcanvas', '[data-offcanvas]', function () {
+		if ($(window).width() < 440){
+			if ($('#filter-sectors-ul').height() > 0){
+				$('#sectors-accordion-selector').trigger('click');
+			}
+
+			if ($('#filter-attributes-ul').height() > 0){
+				$('#attributes-accordion-selector').trigger('click');
+			}
+
+			if ($('#filter-tags-ul').height() > 0){
+				$('#tags-accordion-selector').trigger('click');
+			}
+		}
 	});
 };
