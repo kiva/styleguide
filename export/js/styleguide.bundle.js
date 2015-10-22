d4d71d9205b84447e76e848bb5a05bb5bd452bb5
Abhishek Bhatnagar <abhishekbh@gmail.com>
2015-10-22 00:17:42 -0700

    release 1.0.193

--- export/js/styleguide.bundle.js
+++ export/js/styleguide.bundle.js
@@ -22826,9 +22826,8 @@
 
 			// collapse all accordions if stepping into mobile mode
 			if (Foundation.utils.is_small_only()){
-				var $this = $(this);
-				if ($this.attr('aria-expanded') === 'true'){
-					$this.trigger('click');
+				if ($(this).attr('aria-expanded') === 'true'){
+					$(this).trigger('click');
 				}
 			}
 		}, 200));
