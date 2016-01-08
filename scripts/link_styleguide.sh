da9cbb32e1475ffd598952138575472e5a808733
Steven Magelowitz <steven.magelowitz@gmail.com>
2016-01-08 10:22:32 -0800

    add instructions to undo bower link

--- scripts/link_styleguide.sh
+++ scripts/link_styleguide.sh
@@ -3,4 +3,5 @@ bower link
 cd /kiva/main/sites/www_kiva
 bower uninstall styleguide
 bower link styleguide
-
+echo "To undo, and you have not edited your kiva/main repo:"
+echo "cd /kiva/main & git checkout -f"
