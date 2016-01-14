e0f12975f4ddbb81cadc17924be82a96750d390e
Abhishek Bhatnagar <abhishekbh@gmail.com>
2016-01-14 12:16:43 -0800

    release 1.0.469

--- export/js/styleguide.bundle.js
+++ export/js/styleguide.bundle.js
@@ -46,13 +46,13 @@
 
 	'use strict';
 
-	__webpack_require__(8);
+	__webpack_require__(7);
 
 	var header = __webpack_require__(5);
-	var filters = __webpack_require__(13);
-	var imagesizes = __webpack_require__(14);
+	var filters = __webpack_require__(12);
+	var imagesizes = __webpack_require__(13);
 	var accordion = __webpack_require__(6);
-	var videoResizing = __webpack_require__(7);
+	var videoResizing = __webpack_require__(14);
 	var borrowerPage = __webpack_require__(15);
 	var categories = __webpack_require__(16);
 
@@ -8307,30 +8307,18 @@
 /* 7 */
 /***/ function(module, exports, __webpack_require__) {
 
-	var $ = __webpack_require__(2);
-
-	module.exports = function () {
-	    'use strict';
-
-	    $('.loan-image-wrap').fitVids();
-	};
-
-/***/ },
-/* 8 */
-/***/ function(module, exports, __webpack_require__) {
-
 	__webpack_require__(2);
 	__webpack_require__(3);
 
 	__webpack_require__(4);
 
+	__webpack_require__(8);
 	__webpack_require__(9);
 	__webpack_require__(10);
 	__webpack_require__(11);
-	__webpack_require__(12);
 
 /***/ },
-/* 9 */
+/* 8 */
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(__webpack_provided_window_dot_jQuery) {/*! noUiSlider - 7.0.10 - 2014-12-27 14:50:46 */
@@ -10651,7 +10639,7 @@
 	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(2)))
 
 /***/ },
-/* 10 */
+/* 9 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;var require;var require;/* WEBPACK VAR INJECTION */(function($) {/*!
@@ -16939,7 +16927,7 @@
 	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(2)))
 
 /***/ },
-/* 11 */
+/* 10 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/*
@@ -19587,7 +19575,7 @@
 
 
 /***/ },
-/* 12 */
+/* 11 */
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(__webpack_provided_window_dot_jQuery) {/*global jQuery */
@@ -19661,7 +19649,7 @@
 	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(2)))
 
 /***/ },
-/* 13 */
+/* 12 */
 /***/ function(module, exports, __webpack_require__) {
 
 	module.exports = function() {
@@ -19743,7 +19731,7 @@
 
 
 /***/ },
-/* 14 */
+/* 13 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var $ = __webpack_require__(2);
@@ -19800,6 +19788,18 @@
 
 	};
 
+
+/***/ },
+/* 14 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var $ = __webpack_require__(2);
+
+	module.exports = function () {
+	    'use strict';
+
+	    $('.loan-image-wrap').fitVids();
+	};
 
 /***/ },
 /* 15 */
