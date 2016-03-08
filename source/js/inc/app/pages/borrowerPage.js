09ca34e0b52d0c3b262279a03cd27638fbde37e2
Zahid Zaman <zahid@kiva.org>
2016-03-08 15:50:08 -0800

    REDO-1165 Getting show more toggling working.

--- source/js/inc/app/pages/borrowerPage.js
+++ source/js/inc/app/pages/borrowerPage.js
@@ -21,7 +21,13 @@ module.exports = function () {
         $('.advanced-repayment-schedule').toggle();
     });
 
-    $('.show-more-lenders-teams, .hide-more-lenders-teams').click(function() {
+    $('.show-more-lenders-teams').click(function() {
+        $(".lenders").append($(".spillover-lenders li").clone(true));
         $('.show-more-lenders-teams').toggle();
+    });
+
+    $('.hide-more-lenders-teams').click(function() {
+        $('.show-more-lenders-teams').toggle();
+        $(".lenders li:nth-child(n+11)").remove();
     });
 };
