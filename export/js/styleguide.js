1fc07dbb83222b0c703874eebc4e9a5ebd8a8a0e
Zahid Zaman <zahid@kiva.org>
2016-03-11 11:59:34 -0800

    release 1.0.683

--- export/js/styleguide.js
+++ export/js/styleguide.js
@@ -49,17 +49,17 @@ define("Styleguide", ["jquery"], function(__WEBPACK_EXTERNAL_MODULE_2__) { retur
 	__webpack_require__(7);
 
 	var header = __webpack_require__(5);
-	var filters = __webpack_require__(12);
-	var imagesizes = __webpack_require__(13);
+	var filters = __webpack_require__(13);
+	var imagesizes = __webpack_require__(14);
 	var accordion = __webpack_require__(6);
-	var videoResizing = __webpack_require__(14);
-	var borrowerPage = __webpack_require__(15);
-	var categories = __webpack_require__(16);
-	var lightbox = __webpack_require__(17);
-	var saveSearchLightbox = __webpack_require__(18);
+	var videoResizing = __webpack_require__(15);
+	var borrowerPage = __webpack_require__(16);
+	var categories = __webpack_require__(17);
+	var lightbox = __webpack_require__(18);
+	var saveSearchLightbox = __webpack_require__(19);
 
 	var $ = __webpack_require__(2);
-	var FastClick = __webpack_require__(19);
+	var FastClick = __webpack_require__(20);
 
 	$(document).foundation({
 		equalizer: {
@@ -1509,7 +1509,8 @@ define("Styleguide", ["jquery"], function(__WEBPACK_EXTERNAL_MODULE_2__) { retur
 
 
 	/*** EXPORTS FROM exports-loader ***/
-	module.exports = window.Modernizr}.call(window));
+	module.exports = window.Modernizr
+	}.call(window));
 
 /***/ },
 /* 4 */
@@ -8325,6 +8326,7 @@ define("Styleguide", ["jquery"], function(__WEBPACK_EXTERNAL_MODULE_2__) { retur
 	__webpack_require__(9);
 	__webpack_require__(10);
 	__webpack_require__(11);
+	__webpack_require__(12);
 
 /***/ },
 /* 8 */
@@ -19661,6 +19663,255 @@ define("Styleguide", ["jquery"], function(__WEBPACK_EXTERNAL_MODULE_2__) { retur
 /* 12 */
 /***/ function(module, exports, __webpack_require__) {
 
+	/* WEBPACK VAR INJECTION */(function(jQuery) {/*!
+	 * Lazy Load - jQuery plugin for lazy loading images
+	 *
+	 * Copyright (c) 2007-2015 Mika Tuupola
+	 *
+	 * Licensed under the MIT license:
+	 *   http://www.opensource.org/licenses/mit-license.php
+	 *
+	 * Project home:
+	 *   http://www.appelsiini.net/projects/lazyload
+	 *
+	 * Version:  1.9.7
+	 *
+	 */
+
+	(function($, window, document, undefined) {
+	    var $window = $(window);
+
+	    $.fn.lazyload = function(options) {
+	        var elements = this;
+	        var $container;
+	        var settings = {
+	            threshold       : 0,
+	            failure_limit   : 0,
+	            event           : "scroll",
+	            effect          : "show",
+	            container       : window,
+	            data_attribute  : "original",
+	            skip_invisible  : false,
+	            appear          : null,
+	            load            : null,
+	            placeholder     : "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXYzh8+PB/AAffA0nNPuCLAAAAAElFTkSuQmCC"
+	        };
+
+	        function update() {
+	            var counter = 0;
+
+	            elements.each(function() {
+	                var $this = $(this);
+	                if (settings.skip_invisible && !$this.is(":visible")) {
+	                    return;
+	                }
+	                if ($.abovethetop(this, settings) ||
+	                    $.leftofbegin(this, settings)) {
+	                        /* Nothing. */
+	                } else if (!$.belowthefold(this, settings) &&
+	                    !$.rightoffold(this, settings)) {
+	                        $this.trigger("appear");
+	                        /* if we found an image we'll load, reset the counter */
+	                        counter = 0;
+	                } else {
+	                    if (++counter > settings.failure_limit) {
+	                        return false;
+	                    }
+	                }
+	            });
+
+	        }
+
+	        if(options) {
+	            /* Maintain BC for a couple of versions. */
+	            if (undefined !== options.failurelimit) {
+	                options.failure_limit = options.failurelimit;
+	                delete options.failurelimit;
+	            }
+	            if (undefined !== options.effectspeed) {
+	                options.effect_speed = options.effectspeed;
+	                delete options.effectspeed;
+	            }
+
+	            $.extend(settings, options);
+	        }
+
+	        /* Cache container as jQuery as object. */
+	        $container = (settings.container === undefined ||
+	                      settings.container === window) ? $window : $(settings.container);
+
+	        /* Fire one scroll event per scroll. Not one scroll event per image. */
+	        if (0 === settings.event.indexOf("scroll")) {
+	            $container.bind(settings.event, function() {
+	                return update();
+	            });
+	        }
+
+	        this.each(function() {
+	            var self = this;
+	            var $self = $(self);
+
+	            self.loaded = false;
+
+	            /* If no src attribute given use data:uri. */
+	            if ($self.attr("src") === undefined || $self.attr("src") === false) {
+	                if ($self.is("img")) {
+	                    $self.attr("src", settings.placeholder);
+	                }
+	            }
+
+	            /* When appear is triggered load original image. */
+	            $self.one("appear", function() {
+	                if (!this.loaded) {
+	                    if (settings.appear) {
+	                        var elements_left = elements.length;
+	                        settings.appear.call(self, elements_left, settings);
+	                    }
+	                    $("<img />")
+	                        .bind("load", function() {
+
+	                            var original = $self.attr("data-" + settings.data_attribute);
+	                            $self.hide();
+	                            if ($self.is("img")) {
+	                                $self.attr("src", original);
+	                            } else {
+	                                $self.css("background-image", "url('" + original + "')");
+	                            }
+	                            $self[settings.effect](settings.effect_speed);
+
+	                            self.loaded = true;
+
+	                            /* Remove image from array so it is not looped next time. */
+	                            var temp = $.grep(elements, function(element) {
+	                                return !element.loaded;
+	                            });
+	                            elements = $(temp);
+
+	                            if (settings.load) {
+	                                var elements_left = elements.length;
+	                                settings.load.call(self, elements_left, settings);
+	                            }
+	                        })
+	                        .attr("src", $self.attr("data-" + settings.data_attribute));
+	                }
+	            });
+
+	            /* When wanted event is triggered load original image */
+	            /* by triggering appear.                              */
+	            if (0 !== settings.event.indexOf("scroll")) {
+	                $self.bind(settings.event, function() {
+	                    if (!self.loaded) {
+	                        $self.trigger("appear");
+	                    }
+	                });
+	            }
+	        });
+
+	        /* Check if something appears when window is resized. */
+	        $window.bind("resize", function() {
+	            update();
+	        });
+
+	        /* With IOS5 force loading images when navigating with back button. */
+	        /* Non optimal workaround. */
+	        if ((/(?:iphone|ipod|ipad).*os 5/gi).test(navigator.appVersion)) {
+	            $window.bind("pageshow", function(event) {
+	                if (event.originalEvent && event.originalEvent.persisted) {
+	                    elements.each(function() {
+	                        $(this).trigger("appear");
+	                    });
+	                }
+	            });
+	        }
+
+	        /* Force initial check if images should appear. */
+	        $(document).ready(function() {
+	            update();
+	        });
+
+	        return this;
+	    };
+
+	    /* Convenience methods in jQuery namespace.           */
+	    /* Use as  $.belowthefold(element, {threshold : 100, container : window}) */
+
+	    $.belowthefold = function(element, settings) {
+	        var fold;
+
+	        if (settings.container === undefined || settings.container === window) {
+	            fold = (window.innerHeight ? window.innerHeight : $window.height()) + $window.scrollTop();
+	        } else {
+	            fold = $(settings.container).offset().top + $(settings.container).height();
+	        }
+
+	        return fold <= $(element).offset().top - settings.threshold;
+	    };
+
+	    $.rightoffold = function(element, settings) {
+	        var fold;
+
+	        if (settings.container === undefined || settings.container === window) {
+	            fold = $window.width() + $window.scrollLeft();
+	        } else {
+	            fold = $(settings.container).offset().left + $(settings.container).width();
+	        }
+
+	        return fold <= $(element).offset().left - settings.threshold;
+	    };
+
+	    $.abovethetop = function(element, settings) {
+	        var fold;
+
+	        if (settings.container === undefined || settings.container === window) {
+	            fold = $window.scrollTop();
+	        } else {
+	            fold = $(settings.container).offset().top;
+	        }
+
+	        return fold >= $(element).offset().top + settings.threshold  + $(element).height();
+	    };
+
+	    $.leftofbegin = function(element, settings) {
+	        var fold;
+
+	        if (settings.container === undefined || settings.container === window) {
+	            fold = $window.scrollLeft();
+	        } else {
+	            fold = $(settings.container).offset().left;
+	        }
+
+	        return fold >= $(element).offset().left + settings.threshold + $(element).width();
+	    };
+
+	    $.inviewport = function(element, settings) {
+	         return !$.rightoffold(element, settings) && !$.leftofbegin(element, settings) &&
+	                !$.belowthefold(element, settings) && !$.abovethetop(element, settings);
+	     };
+
+	    /* Custom selectors for your convenience.   */
+	    /* Use as $("img:below-the-fold").something() or */
+	    /* $("img").filter(":below-the-fold").something() which is faster */
+
+	    $.extend($.expr[":"], {
+	        "below-the-fold" : function(a) { return $.belowthefold(a, {threshold : 0}); },
+	        "above-the-top"  : function(a) { return !$.belowthefold(a, {threshold : 0}); },
+	        "right-of-screen": function(a) { return $.rightoffold(a, {threshold : 0}); },
+	        "left-of-screen" : function(a) { return !$.rightoffold(a, {threshold : 0}); },
+	        "in-viewport"    : function(a) { return $.inviewport(a, {threshold : 0}); },
+	        /* Maintain BC for couple of versions. */
+	        "above-the-fold" : function(a) { return !$.belowthefold(a, {threshold : 0}); },
+	        "right-of-fold"  : function(a) { return $.rightoffold(a, {threshold : 0}); },
+	        "left-of-fold"   : function(a) { return !$.rightoffold(a, {threshold : 0}); }
+	    });
+
+	})(jQuery, window, document);
+
+	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(2)))
+
+/***/ },
+/* 13 */
+/***/ function(module, exports, __webpack_require__) {
+
 	module.exports = function() {
 		'use strict';
 
@@ -19748,7 +19999,7 @@ define("Styleguide", ["jquery"], function(__WEBPACK_EXTERNAL_MODULE_2__) { retur
 
 
 /***/ },
-/* 13 */
+/* 14 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var $ = __webpack_require__(2);
@@ -19807,7 +20058,7 @@ define("Styleguide", ["jquery"], function(__WEBPACK_EXTERNAL_MODULE_2__) { retur
 
 
 /***/ },
-/* 14 */
+/* 15 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var $ = __webpack_require__(2);
@@ -19819,7 +20070,7 @@ define("Styleguide", ["jquery"], function(__WEBPACK_EXTERNAL_MODULE_2__) { retur
 	};
 
 /***/ },
-/* 15 */
+/* 16 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var $ = __webpack_require__(2);
@@ -19847,6 +20098,10 @@ define("Styleguide", ["jquery"], function(__WEBPACK_EXTERNAL_MODULE_2__) { retur
 
 	    $('.show-more-lenders-teams').click(function() {
 	        $('.lenders').append($('.spillover-lenders li').clone(true));
+
+			$('.lenders li a span').lazyload({
+				threshold : 200
+			});
 	        $('.show-more-lenders-teams').toggle();
 	    });
 
@@ -19854,6 +20109,10 @@ define("Styleguide", ["jquery"], function(__WEBPACK_EXTERNAL_MODULE_2__) { retur
 	        $('.show-more-lenders-teams').toggle();
 	        $('.lenders li:nth-child(n+11)').remove();
 	    });
+
+		$('.teams li a span').lazyload({
+			threshold : 200
+		});
 
 		// button to reveal or hide the loan tag options
 		var tagToggler = $('.tag-toggler');
@@ -19876,7 +20135,7 @@ define("Styleguide", ["jquery"], function(__WEBPACK_EXTERNAL_MODULE_2__) { retur
 	};
 
 /***/ },
-/* 16 */
+/* 17 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var $ = __webpack_require__(2);
@@ -19900,7 +20159,7 @@ define("Styleguide", ["jquery"], function(__WEBPACK_EXTERNAL_MODULE_2__) { retur
 	};
 
 /***/ },
-/* 17 */
+/* 18 */
 /***/ function(module, exports, __webpack_require__) {
 
 	module.exports = function() {
@@ -19921,7 +20180,7 @@ define("Styleguide", ["jquery"], function(__WEBPACK_EXTERNAL_MODULE_2__) { retur
 	};
 
 /***/ },
-/* 18 */
+/* 19 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var $ = __webpack_require__(2);
@@ -19938,7 +20197,7 @@ define("Styleguide", ["jquery"], function(__WEBPACK_EXTERNAL_MODULE_2__) { retur
 	};
 
 /***/ },
-/* 19 */
+/* 20 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var __WEBPACK_AMD_DEFINE_RESULT__;;(function () {
