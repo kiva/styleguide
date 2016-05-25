628715d79b970505df3caebd585c5213638c8320
Angie <angie.visnesky@gmail.com>
2016-05-25 11:37:21 -0400

    Refactor labs video mustache and associated files to be reusable

--- source/js/inc/app/autoplay-video.js
+++ source/js/inc/app/autoplay-video.js
@@ -6,16 +6,18 @@ module.exports = function () {
 	//Add event handlers to the video play button and video modal close button
   //This adds and removes the autoplay attribute in the video source
 
-  var kivaLabsYoutubeVideo = $('#kivalabsYoutubeVideo');
-  var kivaLabsPlayButton = $('#kivalabsPlayButton');
-  var kivaLabsStopButton = $('#kivalabsStopButton');
+  var autoplayYoutubeVideo = $('#autoplayYoutubeVideo');
+  var autoplayPlayButton = $('#autoplayPlayButton');
+  var autoplayStopButton = $('#autoplayStopButton');
+  var url =  autoplayYoutubeVideo.attr('src');
+  var control = '&autoplay=1';
 
-  kivaLabsPlayButton.click(function(){
-    kivaLabsYoutubeVideo.attr('src', 'http://www.youtube.com/embed/v2vetkuQivw?controls=0&autoplay=1');
+  autoplayPlayButton.click(function(){
+    autoplayYoutubeVideo.attr('src', url + control);
   });
 
-  kivaLabsStopButton.click(function(){
-    kivaLabsYoutubeVideo.attr('src', 'http://www.youtube.com/embed/v2vetkuQivw?controls=0');
+  autoplayStopButton.click(function(){
+    autoplayYoutubeVideo.attr('src', 'src');
   });
 
 };
