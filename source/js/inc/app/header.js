0a3e9f980e334870c75c0c48c9d07402d6d5783f
Jim Edlin <jim@kiva.org>
2016-08-03 15:50:00 -0700

    LW-207 - close header dropdowns on btn click

--- source/js/inc/app/header.js
+++ source/js/inc/app/header.js
@@ -114,6 +114,10 @@ module.exports = function () {
 			.trigger(hidden ? 'hide' : 'show');
 	});
 
+	// close lend dropdown when lend button itself is clicked
+	$('[data-dropdown="lend-dropdown"]').click(function () {
+		Foundation.libs.dropdown.close($('#lend-dropdown'));
+	});
 
 	// close window when normal links clicked
 	$('#lend-dropdown a:not([data-kv-toggle],[href="#"])').click(function () {
@@ -131,5 +135,15 @@ module.exports = function () {
 		// lend-menu-small
 		$('.lend-menu-small li>a').attr('aria-expanded', false);
 		$('.lend-menu-small ul').attr('aria-hidden', true).css('height', 0);
+	});
+
+	// close about dropdown when about button itself is clicked
+	$('[data-dropdown="about-dropdown"]').click(function () {
+		Foundation.libs.dropdown.close($('#about-dropdown'));
+	});
+
+	// close my-kiva dropdown when my-kiva button itself is clicked
+	$('[data-dropdown="my-kiva-dropdown"]').click(function () {
+		Foundation.libs.dropdown.close($('#my-kiva-dropdown'));
 	});
 };
