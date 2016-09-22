9b688299fdf87125d51ab4209b61f493c2a02765
Zahid Zaman <zahid@kiva.org>
2016-09-22 11:18:35 -0700

    release 1.0.934

--- export/js/styleguide.bundle.js
+++ export/js/styleguide.bundle.js
@@ -1516,7 +1516,7 @@
 
 
 	/*** EXPORTS FROM exports-loader ***/
-	module.exports = window.Modernizr;
+	module.exports = window.Modernizr
 	}.call(window));
 
 /***/ },
@@ -8107,7 +8107,7 @@
 
 
 	/*** EXPORTS FROM exports-loader ***/
-	module.exports = window.Foundation;
+	module.exports = window.Foundation
 	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(2)))
 
 /***/ },
@@ -10726,7 +10726,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;var require;var require;/* WEBPACK VAR INJECTION */(function($) {/*!
-	 * Select2 4.0.2
+	 * Select2 4.0.1
 	 * https://select2.github.io
 	 *
 	 * Released under the MIT license
@@ -11869,7 +11869,11 @@
 	      this.$results.on('mousewheel', function (e) {
 	        var top = self.$results.scrollTop();
 
-	        var bottom = self.$results.get(0).scrollHeight - top + e.deltaY;
+	        var bottom = (
+	          self.$results.get(0).scrollHeight -
+	          self.$results.scrollTop() +
+	          e.deltaY
+	        );
 
 	        var isAtTop = e.deltaY > 0 && top - e.deltaY <= 0;
 	        var isAtBottom = e.deltaY < 0 && bottom <= self.$results.height();
@@ -14055,7 +14059,7 @@
 	        var $existingOption = $existing.filter(onlyItem(item));
 
 	        var existingData = this.item($existingOption);
-	        var newData = $.extend(true, {}, item, existingData);
+	        var newData = $.extend(true, {}, existingData, item);
 
 	        var $newOption = this.option(newData);
 
@@ -14163,9 +14167,7 @@
 
 	        callback(results);
 	      }, function () {
-	        self.trigger('results:message', {
-	          message: 'errorLoading'
-	        });
+	        // TODO: Handle AJAX errors
 	      });
 
 	      self._request = $request;
@@ -14195,12 +14197,6 @@
 
 	    if (createTag !== undefined) {
 	      this.createTag = createTag;
-	    }
-
-	    var insertTag = options.get('insertTag');
-
-	    if (insertTag !== undefined) {
-	        this.insertTag = insertTag;
 	    }
 
 	    decorated.call(this, $element, options);
@@ -14902,6 +14898,7 @@
 
 	    var newDirection = null;
 
+	    var position = this.$container.position();
 	    var offset = this.$container.offset();
 
 	    offset.bottom = offset.top + this.$container.outerHeight(false);
@@ -14930,19 +14927,13 @@
 	      top: container.bottom
 	    };
 
-	    // Determine what the parent element is to use for calciulating the offset
-	    var $offsetParent = this.$dropdownParent;
+	    // Fix positioning with static parents
+	    if (this.$dropdownParent[0].style.position !== 'static') {
+	      var parentOffset = this.$dropdownParent.offset();
 
-	    // For statically positoned elements, we need to get the element
-	    // that is determining the offset
-	    if ($offsetParent.css('position') === 'static') {
-	      $offsetParent = $offsetParent.offsetParent();
+	      css.top -= parentOffset.top;
+	      css.left -= parentOffset.left;
 	    }
-
-	    var parentOffset = $offsetParent.offset();
-
-	    css.top -= parentOffset.top;
-	    css.left -= parentOffset.left;
 
 	    if (!isCurrentlyAbove && !isCurrentlyBelow) {
 	      newDirection = 'below';
@@ -15210,7 +15201,7 @@
 	  }
 
 	  Defaults.prototype.apply = function (options) {
-	    options = $.extend(true, {}, this.defaults, options);
+	    options = $.extend({}, this.defaults, options);
 
 	    if (options.dataAdapter == null) {
 	      if (options.ajax != null) {
@@ -15774,7 +15765,6 @@
 	      id = Utils.generateChars(4);
 	    }
 
-	    id = id.replace(/(:|\.|\[|\]|,)/g, '');
 	    id = 'select2-' + id;
 
 	    return id;
@@ -17031,7 +17021,7 @@
 	|___/_|_|\___|_|\_(_)/ |___/
 	                   |__/
 
-	 Version: 1.5.9
+	 Version: 1.5.8
 	  Author: Ken Wheeler
 	 Website: http://kenwheeler.github.io
 	    Docs: http://kenwheeler.github.io/slick
@@ -17106,7 +17096,6 @@
 	                touchMove: true,
 	                touchThreshold: 5,
 	                useCSS: true,
-	                useTransform: false,
 	                variableWidth: false,
 	                vertical: false,
 	                verticalSwiping: false,
@@ -17525,6 +17514,8 @@
 	                .attr('data-slick-index', index)
 	                .data('originalStyling', $(element).attr('style') || '');
 	        });
+
+	        _.$slidesCache = _.$slides;
 
 	        _.$slider.addClass('slick-slider');
 
@@ -17991,8 +17982,6 @@
 
 	        if (filter !== null) {
 
-	            _.$slidesCache = _.$slides;
-
 	            _.unload();
 
 	            _.$slideTrack.children(this.options.slide).detach();
@@ -18023,7 +18012,7 @@
 	        if (_.options.infinite === true) {
 	            while (breakPoint < _.slideCount) {
 	                ++pagerQty;
-	                breakPoint = counter + _.options.slidesToScroll;
+	                breakPoint = counter + _.options.slidesToShow;
 	                counter += _.options.slidesToScroll <= _.options.slidesToShow ? _.options.slidesToScroll : _.options.slidesToShow;
 	            }
 	        } else if (_.options.centerMode === true) {
@@ -18031,7 +18020,7 @@
 	        } else {
 	            while (breakPoint < _.slideCount) {
 	                ++pagerQty;
-	                breakPoint = counter + _.options.slidesToScroll;
+	                breakPoint = counter + _.options.slidesToShow;
 	                counter += _.options.slidesToScroll <= _.options.slidesToShow ? _.options.slidesToScroll : _.options.slidesToShow;
 	            }
 	        }
@@ -18100,33 +18089,15 @@
 	                targetSlide = _.$slideTrack.children('.slick-slide').eq(slideIndex + _.options.slidesToShow);
 	            }
 
-	            if (_.options.rtl === true) {
-	                if (targetSlide[0]) {
-	                    targetLeft = (_.$slideTrack.width() - targetSlide[0].offsetLeft - targetSlide.width()) * -1;
-	                } else {
-	                    targetLeft =  0;
-	                }
-	            } else {
-	                targetLeft = targetSlide[0] ? targetSlide[0].offsetLeft * -1 : 0;
-	            }
+	            targetLeft = targetSlide[0] ? targetSlide[0].offsetLeft * -1 : 0;
 
 	            if (_.options.centerMode === true) {
-	                if (_.slideCount <= _.options.slidesToShow || _.options.infinite === false) {
+	                if (_.options.infinite === false) {
 	                    targetSlide = _.$slideTrack.children('.slick-slide').eq(slideIndex);
 	                } else {
 	                    targetSlide = _.$slideTrack.children('.slick-slide').eq(slideIndex + _.options.slidesToShow + 1);
 	                }
-
-	                if (_.options.rtl === true) {
-	                    if (targetSlide[0]) {
-	                        targetLeft = (_.$slideTrack.width() - targetSlide[0].offsetLeft - targetSlide.width()) * -1;
-	                    } else {
-	                        targetLeft =  0;
-	                    }
-	                } else {
-	                    targetLeft = targetSlide[0] ? targetSlide[0].offsetLeft * -1 : 0;
-	                }
-
+	                targetLeft = targetSlide[0] ? targetSlide[0].offsetLeft * -1 : 0;
 	                targetLeft += (_.$list.width() - targetSlide.outerWidth()) / 2;
 	            }
 	        }
@@ -18538,7 +18509,6 @@
 
 	        if (imgCount > 0) {
 	            targetImage = $('img[data-lazy]', _.$slider).first();
-	            targetImage.attr('src', null);
 	            targetImage.attr('src', targetImage.attr('data-lazy')).removeClass('slick-loading').load(function() {
 	                    targetImage.removeAttr('data-lazy');
 	                    _.progressiveLazyLoad();
@@ -18557,22 +18527,8 @@
 
 	    Slick.prototype.refresh = function( initializing ) {
 
-	        var _ = this, currentSlide, firstVisible;
-
-	        firstVisible = _.slideCount - _.options.slidesToShow;
-
-	        // check that the new breakpoint can actually accept the
-	        // "current slide" as the current slide, otherwise we need
-	        // to set it to the closest possible value.
-	        if ( !_.options.infinite ) {
-	            if ( _.slideCount <= _.options.slidesToShow ) {
-	                _.currentSlide = 0;
-	            } else if ( _.currentSlide > firstVisible ) {
-	                _.currentSlide = firstVisible;
-	            }
-	        }
-
-	         currentSlide = _.currentSlide;
+	        var _ = this,
+	            currentSlide = _.currentSlide;
 
 	        _.destroy(true);
 
@@ -18951,7 +18907,8 @@
 	            _.transformType = 'transform';
 	            _.transitionType = 'transition';
 	        }
-	        _.transformsEnabled = _.options.useTransform && (_.animType !== null && _.animType !== false);
+	        _.transformsEnabled = (_.animType !== null && _.animType !== false);
+
 	    };
 
 
@@ -19646,13 +19603,18 @@
 	    };
 
 	    Slick.prototype.activateADA = function() {
-	        var _ = this;
+	        var _ = this,
+	        _isSlideOnFocus =_.$slider.find('*').is(':focus');
+	        // _isSlideOnFocus = _.$slides.is(':focus') || _.$slides.find('*').is(':focus');
 
 	        _.$slideTrack.find('.slick-active').attr({
-	            'aria-hidden': 'false'
+	            'aria-hidden': 'false',
+	            'tabindex': '0'
 	        }).find('a, input, button, select').attr({
 	            'tabindex': '0'
 	        });
+
+	        (_isSlideOnFocus) &&  _.$slideTrack.find('.slick-active').focus();
 
 	    };
 
@@ -19680,9 +19642,9 @@
 	            opt = arguments[0],
 	            args = Array.prototype.slice.call(arguments, 1),
 	            l = _.length,
-	            i,
+	            i = 0,
 	            ret;
-	        for (i = 0; i < l; i++) {
+	        for (i; i < l; i++) {
 	            if (typeof opt == 'object' || typeof opt == 'undefined')
 	                _[i].slick = new Slick(_[i], opt);
 	            else
