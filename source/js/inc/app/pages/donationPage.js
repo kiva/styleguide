8363ef835b28b6b0ea131d8db9fbec6ac709ebab
joshua starkey <joshua@kiva.org>
2016-10-18 03:01:35 -0700

    more work toward the donation form.

---
+++ source/js/inc/app/pages/donationPage.js
@@ -0,0 +1,34 @@
+var $ = require('jquery');
+
+module.exports = function () {
+	'use strict';
+
+	//Donation amount selection buttons
+	//1- I need to have the $50 amount selected upon pageload
+	//2- I need to add mouse over and click events to all buttons
+	//3- I need to figure out how to do the input field
+	//4-
+
+	//selecting a donation amount button
+	/*$('.amountButton')(function(e)  {
+		e.preventDefault();
+		$(this).toggleClass('active');
+		/!*$('.amountButton').addClass('.selected');*!/
+	});
+*/
+	$('.amountButton').on('click', function () {
+		$(this).addClass('.selected');
+
+		/*
+		$('.top_menu span').removeClass('active');
+		$(this).addClass('active');
+*/
+
+	});
+
+	//Donate button
+	//1- If monthly donation box is checked: direct user to /about/supportus/monthly
+	//   If monthly donation box is not checked: direct user to /basket with donation about selected or entered inside of their basket
+
+
+}
