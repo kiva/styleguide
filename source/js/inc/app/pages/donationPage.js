711402f86a1d89698c5f46c261acc889ba159b8c
joshua starkey <joshua@kiva.org>
2016-11-03 15:01:37 -0700

    donate form work. js

--- source/js/inc/app/pages/donationPage.js
+++ source/js/inc/app/pages/donationPage.js
@@ -4,18 +4,49 @@ module.exports = function () {
 	'use strict';
 
 	//Donation amount selection buttons
-	//1- I need to have the $50 amount selected upon pageload
-	//2- I need to add mouse over and click events to all buttons
-	//3- I need to figure out how to do the input field
-	//4-
+	//1- DONE I need to have the $50 amount selected upon pageload
+	//2- DONE I need to add mouse over and click events to all buttons
+	//3- DONE I need to only allow numbers in the input field
+	//4- DONE I need to deselect a button when the input is selected.
+	//5- 1/2 DONE (need advice moving forward with this one.) I need to add a leading dollar sign in the input box when user enters an amount
+	//6- I need to add values to the buttons and input that can be passed through when the Donate button is clicked
 
-	$( function() {
-		$('#donation-buttons').selectable();
+	// This is deselecting a radio button if one is selected when user clicks the input field.
+	$('#donation-amount-input').click(function() {
+
+		//This only works when the type of the input is not set to type="number", but then the field accepts letter
+		//characters, which is not ideal.
+		/*
+		var text = $('#donation-amount-input');
+		text.val('$');
+		*/
+
+		var checked = $('.donation-amount-button').attr('checked', true);
+		if(checked){
+			$('.donation-amount-button').attr('checked', false);
+		}
 	});
 
+	// This is clearing the input field if user has entered a value and clicks on a radio button
+	$('.donation-amount-button').click(function () {
+		if ($('#donation-amount-input').val() > 0) {
+			$('#donation-amount-input').val('');
+		}
+	});
+
+
 	//Donate button
-	//1- If monthly donation box is checked: direct user to /about/supportus/monthly
-	//   If monthly donation box is not checked: direct user to /basket with donation about selected or entered inside of their basket
+	$('.donate-submit').click(function() {
+		if ($('#makeDonationMonthly').is(":checked")) {
+			window.location.href = "http://www.kiva.org/about/supportus/monthly";
+			console.log("support us page direct");
 
+		} else {
+			/*direct user to /basket with donation amount selected or entered inside of their basket*/
+			window.location.href = "http://www.kiva.org/basket";
+			console.log("basket page direct");
+			//need to bring the donation value with me somehow.
+		}
 
+	});
 };
