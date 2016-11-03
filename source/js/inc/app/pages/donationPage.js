ce0f55cf98fa8da172f10667ce1fc688df19237d
joshua starkey <joshua@kiva.org>
2016-11-03 16:48:41 -0700

    javascript work toward the donation page.

--- source/js/inc/app/pages/donationPage.js
+++ source/js/inc/app/pages/donationPage.js
@@ -8,45 +8,71 @@ module.exports = function () {
 	//2- DONE I need to add mouse over and click events to all buttons
 	//3- DONE I need to only allow numbers in the input field
 	//4- DONE I need to deselect a button when the input is selected.
-	//5- 1/2 DONE (need advice moving forward with this one.) I need to add a leading dollar sign in the input box when user enters an amount
-	//6- I need to add values to the buttons and input that can be passed through when the Donate button is clicked
+	//5- DONE I need to add a leading dollar sign in the input box when user enters an amount
+	//6- DONE I need to add values to the buttons and input that can be passed through when the Donate button is clicked
 
 	// This is deselecting a radio button if one is selected when user clicks the input field.
 	$('#donation-amount-input').click(function() {
-
-		//This only works when the type of the input is not set to type="number", but then the field accepts letter
-		//characters, which is not ideal.
-		/*
-		var text = $('#donation-amount-input');
-		text.val('$');
-		*/
-
 		var checked = $('.donation-amount-button').attr('checked', true);
 		if(checked){
 			$('.donation-amount-button').attr('checked', false);
 		}
+		//This adds a leading $ to the input field when user clicks on it.
+		var text = $('#donation-amount-input');
+		text.val('$');
 	});
 
 	// This is clearing the input field if user has entered a value and clicks on a radio button
-	$('.donation-amount-button').click(function () {
-		if ($('#donation-amount-input').val() > 0) {
+	$('.donation-amount-button').click(function() {
+		if ($('#donation-amount-input').length > 0) {
 			$('#donation-amount-input').val('');
+			console.log('Input field should have cleared.');
 		}
 	});
 
 
 	//Donate button
-	$('.donate-submit').click(function() {
+	$('#donation-form').submit(function(e) {
+		e.preventDefault();
+
+		//This is verifying the donation input is a valid currency amount. Commas and decimals are optional.
+		var donationValue = document.forms['donation-form']['donation-amount-input'].value;
+		var regex = /(?=.)^\$?(([1-9][0-9]{0,2}(,[0-9]{3})*)|[0-9]+)?(\.[0-9]{1,2})?$/;
+		if (!donationValue.match(regex)) {
+			console.log('Donation amount must be numbers only.');
+			//if entered donation amount is not valid, clear the field.
+			$('#donation-amount-input').val('');
+			return false;
+		}
+
+		//This controls where we send the user after clicking the donate button.
+		console.log('form submitted.');
 		if ($('#makeDonationMonthly').is(':checked')) {
 			window.location.href = 'http://www.kiva.org/about/supportus/monthly';
-			console.log('support us page direct');
-
+			//need to direct user to the /supportus/monthly page
 		} else {
 			/*direct user to /basket with donation amount selected or entered inside of their basket*/
 			window.location.href = 'http://www.kiva.org/basket';
-			console.log('basket page direct');
-			//need to bring the donation value with me somehow.
+			//need to direct the user to the basket page and bring the donation value with me.
 		}
+	});
 
+	//verifying text in donation amount input is numbers
+	function checkDonationValid() {
+		var donationValue = document.forms['donation-form']['donation-amount-input'].value;
+		var regex = /(?=.)^\$?(([1-9][0-9]{0,2}(,[0-9]{3})*)|[0-9]+)?(\.[0-9]{1,2})?$/;
+		console.log('donation value reviewed.');
+		if (donationValue.match(regex)) {
+			alert('Donation amount must be numbers only.');
+			return false;
+		}
+	}
+
+
+	//This is selecting the monthly donation checkbox when a user clicks on the checkbox's label
+	var labelID;
+	$('.donate-monthly-checkbox').click(function() {
+		labelID = $('.donate-monthly-checkbox').attr('for');
+		$(labelID).trigger('click');
 	});
 };
