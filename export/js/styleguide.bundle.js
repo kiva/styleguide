c8bcdb8bd87031326a883d9298320fe20707fa1f
joshua starkey <joshua@kiva.org>
2016-11-08 11:03:00 -0800

    release 1.1.2

--- export/js/styleguide.bundle.js
+++ export/js/styleguide.bundle.js
@@ -57,9 +57,10 @@
 	var categories = __webpack_require__(17);
 	var lightbox = __webpack_require__(18);
 	var saveSearchLightbox = __webpack_require__(19);
+	var donationPage = __webpack_require__(20);
 
 	var $ = __webpack_require__(2);
-	var FastClick = __webpack_require__(20);
+	var FastClick = __webpack_require__(21);
 
 	$(document).ready(function() {
 		$(document).foundation({
@@ -90,6 +91,7 @@
 		categories();
 		lightbox();
 		saveSearchLightbox();
+		donationPage();
 	});
 
 
@@ -20533,6 +20535,31 @@
 
 /***/ },
 /* 20 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var $ = __webpack_require__(2);
+
+	module.exports = function () {
+		'use strict';
+
+		// This is deselecting a radio button if one is selected when user clicks the input field.
+		$('#donation-amount-input').click(function() {
+			var checked = $('.donation-amount-button').attr('checked', true);
+			if(checked){
+				$('.donation-amount-button').attr('checked', false);
+			}
+		});
+
+		// This is clearing the input field if user has entered a value and clicks on a radio button
+		$('.donation-amount-button').click(function() {
+			if ($('#donation-amount-input').length > 0) {
+				$('#donation-amount-input').val('');
+			}
+		});
+	};
+
+/***/ },
+/* 21 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var __WEBPACK_AMD_DEFINE_RESULT__;;(function () {
