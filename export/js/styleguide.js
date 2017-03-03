1f7ee53f98a36cd311639c6d12fd3ac6916d12dc
Joshua Kiwiet-Pantaleoni <emuvente@gmail.com>
2017-03-02 20:06:20 -0800

    release 1.1.114

--- export/js/styleguide.js
+++ export/js/styleguide.js
@@ -20538,12 +20538,12 @@ define("Styleguide", ["jquery"], function(__WEBPACK_EXTERNAL_MODULE_2__) { retur
 		var $body = $('body');
 
 		//Blocking scrolling on the body of a page when a lightbox opens
-		$(document).on('open.fndtn.reveal', '[data-reveal]', function () {
+		$(document).on('opened.fndtn.reveal', function () {
 			$body.css('overflow', 'hidden');
 		});
 
 		//Allowing scrolling on the body of a page when a lightbox is closed
-		$(document).on('close.fndtn.reveal', '[data-reveal]', function () {
+		$(document).on('closed.fndtn.reveal', function () {
 			$body.css('overflow', 'visible');
 		});
 	};
