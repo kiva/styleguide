bd3cdb5eeff84bbc0996bf41141ce443fd3d0d1d
Joshua Kiwiet-Pantaleoni <emuvente@gmail.com>
2017-03-02 18:59:35 -0800

    fix up lightbox event handling

--- source/js/inc/app/lightbox.js
+++ source/js/inc/app/lightbox.js
@@ -5,12 +5,12 @@ module.exports = function() {
 	var $body = $('body');
 
 	//Blocking scrolling on the body of a page when a lightbox opens
-	$(document).on('open.fndtn.reveal', '[data-reveal]', function () {
+	$(document).on('opened.fndtn.reveal', function () {
 		$body.css('overflow', 'hidden');
 	});
 
 	//Allowing scrolling on the body of a page when a lightbox is closed
-	$(document).on('close.fndtn.reveal', '[data-reveal]', function () {
+	$(document).on('closed.fndtn.reveal', function () {
 		$body.css('overflow', 'visible');
 	});
 };
